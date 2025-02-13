b165d730a4239cc501e59bd58feff240317bafd2
csci40 finalproject <christianolivares13@gmail.com>
2025-02-13 12:27:02 +0800

    zoom features nalang kulang

--- Main.cpp
+++ Main.cpp
@@ -6,36 +6,41 @@
 #include <fstream>
 #include <vector>
 
-
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 const int FPS = 60;
-const int TIMESTEP = 1/FPS;
+const int TIMESTEP = 1 / FPS;
 
-
-struct object{ // @Christian this is what I'm using to store object name and position. -JT
+struct object
+{ // @Christian this is what I'm using to store object name and position. -JT
     std::string objectName;
     Vector2 objectPos;
     bool isFound = false;
     Color color = RED;
 };
 
-void movement(int up, int down, int right, int left, float speed, Vector2& circle){
-    if(IsKeyDown(up)){
-        circle.y -= speed*GetFrameTime();
+void movement(float edge_x[2], float edge_y[2], float speed, Vector2 &circle, float &circle_size)
+{
+    if (IsKeyDown(KEY_W) && (circle.y - circle_size / 2) > edge_y[0])
+    {
+        circle.y -= speed * GetFrameTime();
     }
-    if(IsKeyDown(down)){
-        circle.y += speed*GetFrameTime();
+    if (IsKeyDown(KEY_S) && (circle.y + circle_size / 2) < edge_y[1])
+    {
+        circle.y += speed * GetFrameTime();
     }
-    if(IsKeyDown(right)){
-        circle.x += speed*GetFrameTime();
+    if (IsKeyDown(KEY_A) && (circle.x - circle_size) > edge_x[0])
+    {
+        circle.x -= speed * GetFrameTime();
     }
-    if(IsKeyDown(left)){
-        circle.x -= speed*GetFrameTime();
+    if (IsKeyDown(KEY_D) && (circle.x + circle_size / 2) < edge_x[1])
+    {
+        circle.x += speed * GetFrameTime();
     }
 }
 
-bool IsInSelectionArea(Vector2 point, Vector2 squarePos, float size) {
+bool IsInSelectionArea(Vector2 point, Vector2 squarePos, float size)
+{
     return (point.x >= squarePos.x && point.x <= squarePos.x + size && point.y >= squarePos.y && point.y <= squarePos.y + size);
 }
 
@@ -171,19 +176,19 @@ void parseSettingsFile(std::string &imageName, object &object1, object &object2,
             if (line.find("CameraDrift = ") == 0)
             {
                 searchTarget = "CameraDrift = ";
-                cameraDriftSpeed = (float) std::stoi(line.erase(0, searchTarget.length()));
+                cameraDriftSpeed = (float)std::stoi(line.erase(0, searchTarget.length()));
             }
         }
     }
 }
 
-
-int main(){
+int main()
+{
     bool movementEnabled = true;
     float speed = 220;
-    int fontSize = 16;
-    Vector2 player = {(WINDOW_WIDTH/2) + 100, (WINDOW_HEIGHT/2) + 100};
-    float playerSize = 100;
+    int fontSize = 24;
+    Vector2 player = {(WINDOW_WIDTH / 2) + 100, (WINDOW_HEIGHT / 2) + 100};
+    float playerSize = 200;
     Vector2 cameraWindowDimensions;
     float cameraDriftSpeed;
     std::string imageName;
@@ -193,16 +198,14 @@ int main(){
     object object4;
     object object5;
 
-    
-
     // camera stuff
     int cam_type;
-    Camera2D camera_view = { 0 };
+    Camera2D camera_view = {0};
     float EDGE_X[2], EDGE_Y[2];
 
     // camera settings
     camera_view.target = player;
-    Vector2 defaultOffset = {WINDOW_WIDTH/2, WINDOW_HEIGHT/2};
+    Vector2 defaultOffset = {WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2};
     camera_view.offset = defaultOffset;
     camera_view.zoom = 1.0f;
     int victoryCounter = 0;
@@ -211,59 +214,95 @@ int main(){
     parseSettingsFile(imageName, object1, object2, object3, object4, object5, EDGE_X, EDGE_Y, cameraWindowDimensions, cameraDriftSpeed);
     std::cout << "EDGE_X[0] = " << EDGE_X[0] << " AND EDGE_X[1] = " << EDGE_X[1] << std::endl;
     std::cout << "EDGE_Y[0] = " << EDGE_Y[0] << " AND EDGE_Y[1] = " << EDGE_Y[1] << std::endl;
-    Image displayedImage = LoadImage("toybox.jpg");
+    // Image displayedImage = LoadImage("toybox.jpg");
+    Image displayedImage = LoadImage(imageName.c_str());
     Texture texture = LoadTextureFromImage(displayedImage);
     std::vector<object> allObjects = {object1, object2, object3, object4, object5};
 
-    while(!WindowShouldClose()){
+    while (!WindowShouldClose())
+    {
         victoryCounter = 0;
-        if(movementEnabled){
-            movement(KEY_W, KEY_S, KEY_D, KEY_A, speed, player);
-        }
-        if(IsKeyPressed(KEY_ENTER)){
+        // if (movementEnabled)
+        // {
+        movement(EDGE_X, EDGE_Y, speed, player, playerSize);
+        // }
+
+        if (IsKeyPressed(KEY_ENTER) && movementEnabled)
+        {
+            // Sauce: https://www.raylib.com/examples/core/loader.html?name=core_2d_camera_mouse_zoom
+            // Vector2 mouseWorldPos = GetScreenToWorld2D(player, camera_view);
+            // camera_view.offset = player;
+            // camera_view.target = mouseWorldPos;
+
             camera_view.zoom = 1.4f;
             movementEnabled = false;
         }
-        if(IsKeyPressed(KEY_TAB)){
-            camera_view.zoom = 1.0f;
-            movementEnabled = true;
-        }
-        for (size_t i = 0; i < allObjects.size(); i++){
-            if(CheckCollisionPointRec(allObjects[i].objectPos, (Rectangle){player.x, player.y, playerSize, playerSize})){
-                allObjects[i].isFound = true;
-                allObjects[i].color = GREEN;
+        else if (IsKeyPressed(KEY_ENTER) && !movementEnabled)
+        {
+            for (size_t i = 0; i < allObjects.size(); i++)
+            {
+                if (CheckCollisionPointRec(allObjects[i].objectPos, (Rectangle){player.x, player.y, playerSize, playerSize}))
+                {
+                    allObjects[i].isFound = true;
+                    allObjects[i].color = GREEN;
+                }
             }
         }
 
-        for (size_t i = 0; i < allObjects.size(); i++){
-            if(allObjects[i].isFound == true){
+        if (IsKeyPressed(KEY_TAB))
+        {
+            // Vector2 mouseWorldPos = GetScreenToWorld2D(player, camera_view);
+            // camera_view.offset = player;
+            // camera_view.target = mouseWorldPos;
+
+            camera_view.zoom = 1.0f;
+            movementEnabled = true;
+        }
+
+        for (size_t i = 0; i < allObjects.size(); i++)
+        {
+            if (allObjects[i].isFound == true)
+            {
                 victoryCounter++;
             }
         }
-        
-        
-        if(!(player.x - defaultOffset.x <= EDGE_X[0] || player.x + defaultOffset.x >= EDGE_X[1])){
-            if(player.x <= camera_view.target.x - cameraWindowDimensions.x/2){
-                camera_view.target.x = player.x + cameraWindowDimensions.x/2;
-            }
-            if(player.x + playerSize >= camera_view.target.x + cameraWindowDimensions.x/2){
-                camera_view.target.x = player.x + playerSize - cameraWindowDimensions.x/2;
-            }
-            // camera_view.offset.x = defaultOffset.x;
+
+        // Camera-Window & Position-Snapping is enabled when there's no Edge-Snapping
+        bool hasNoInput = !(IsKeyDown(KEY_W) || IsKeyDown(KEY_A) || IsKeyDown(KEY_S) || IsKeyDown(KEY_D));
+        if (!(camera_view.target.x - (playerSize / 2) - defaultOffset.x <= EDGE_X[0]) && movementEnabled) // < EDGE_X Min
+        {
+            if (player.x - (playerSize / 2) <= camera_view.target.x - cameraWindowDimensions.x / 2)
+                camera_view.target.x = player.x - (playerSize / 2) + cameraWindowDimensions.x / 2;
+            if (player.x < camera_view.target.x && hasNoInput)
+                camera_view.target.x -= cameraDriftSpeed * .05;
         }
-        if(!(player.y - defaultOffset.y <= EDGE_Y[0] || player.y + defaultOffset.y >= EDGE_Y[1])){
-            if(player.y <= camera_view.target.y - cameraWindowDimensions.y/2){
-                camera_view.target.y = player.y + cameraWindowDimensions.y/2;
-            }
-            if(player.y + playerSize >= camera_view.target.y + cameraWindowDimensions.y/2){
-                camera_view.target.y = player.y + playerSize - cameraWindowDimensions.y/2;
-            }
-            // camera_view.offset.y = defaultOffset.y;
+        if (!(camera_view.target.x + defaultOffset.x >= EDGE_X[1]) && movementEnabled) // < EDGE_X Max
+        {
+            if (player.x + (playerSize / 2) >= camera_view.target.x + cameraWindowDimensions.x / 2)
+                camera_view.target.x = player.x + (playerSize / 2) - (cameraWindowDimensions.x / 2);
+            if (player.x > camera_view.target.x && hasNoInput)
+                camera_view.target.x += cameraDriftSpeed * .05;
         }
+
+        if (!(camera_view.target.y - defaultOffset.y <= EDGE_Y[0]) && movementEnabled) // < EDGE_Y Min
+        {
+            if (player.y - (playerSize / 2) <= camera_view.target.y - cameraWindowDimensions.y / 2)
+                camera_view.target.y = player.y - (playerSize / 2) + cameraWindowDimensions.y / 2;
+            if (player.y < camera_view.target.y && hasNoInput)
+                camera_view.target.y -= cameraDriftSpeed * .05;
+        }
+        if (!(camera_view.target.y + defaultOffset.y >= EDGE_Y[1]) && movementEnabled) // < EDGE_Y Max
+        {
+            if (player.y + (playerSize / 2) >= camera_view.target.y + cameraWindowDimensions.y / 2)
+                camera_view.target.y = player.y + (playerSize / 2) - cameraWindowDimensions.y / 2;
+            if (player.y > camera_view.target.y && hasNoInput)
+                camera_view.target.y += cameraDriftSpeed * .05;
+        }
+
         /*
-        */
-        
-        //std::cout << camera_view.target.x << " " << camera_view.target.y<< std::endl;
+         */
+
+        // std::cout << camera_view.target.x << " " << camera_view.target.y<< std::endl;
         /*
         if(player.x - defaultOffset.x <= EDGE_X[0]){
             //camera_view.target.x = camera_view.target.x;
@@ -299,19 +338,32 @@ int main(){
 
         ClearBackground(BLACK);
         DrawTexture(texture, 0, 0, WHITE);
-        DrawRectangleLines((camera_view.target.x) - cameraWindowDimensions.x/2 , (camera_view.target.y) - cameraWindowDimensions.y/2, cameraWindowDimensions.x, cameraWindowDimensions.y, RED);
-        DrawText(object1.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x/2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y/2) - 150, fontSize, allObjects[0].color);
-        DrawText(object2.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x/2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y/2) - 150 + fontSize+1, fontSize, allObjects[1].color);
-        DrawText(object3.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x/2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y/2) - 150 + ((fontSize+1) * 2), fontSize, allObjects[2].color);
-        DrawText(object4.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x/2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y/2) - 150 + ((fontSize+1) * 3), fontSize, allObjects[3].color);
-        DrawText(object5.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x/2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y/2) - 150 + ((fontSize+1) * 4), fontSize, allObjects[4].color);
-        DrawRectangle(player.x, player.y, playerSize, playerSize, PINK);
-        
-        if(victoryCounter == allObjects.size()){
-            DrawText("YOU WIN!",(camera_view.target.x) - cameraWindowDimensions.x/2 , (camera_view.target.y) - cameraWindowDimensions.y/2, 40, BLUE);
-        }
+        if (movementEnabled)
+            DrawRectangleLines((camera_view.target.x) - cameraWindowDimensions.x / 2, (camera_view.target.y) - cameraWindowDimensions.y / 2, cameraWindowDimensions.x, cameraWindowDimensions.y, RED);
+        DrawRectangleLines(player.x - (playerSize / 2), player.y - (playerSize / 2), playerSize, playerSize, PINK);
+        // DrawText(object1.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150, fontSize, allObjects[0].color);
+        // DrawText(object2.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150 + fontSize + 1, fontSize, allObjects[1].color);
+        // DrawText(object3.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150 + ((fontSize + 1) * 2), fontSize, allObjects[2].color);
+        // DrawText(object4.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150 + ((fontSize + 1) * 3), fontSize, allObjects[3].color);
+        // DrawText(object5.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150 + ((fontSize + 1) * 4), fontSize, allObjects[4].color);
+
+        // if (victoryCounter == allObjects.size())
+        // {
+        //     DrawText("YOU WIN!", (camera_view.target.x) - cameraWindowDimensions.x / 2, (camera_view.target.y) - cameraWindowDimensions.y / 2, 40, BLUE);
+        // }
 
         EndMode2D();
+
+        DrawText(object1.objectName.c_str(), 10, 10, fontSize, allObjects[0].color);
+        DrawText(object2.objectName.c_str(), 10, 35, fontSize, allObjects[1].color);
+        DrawText(object3.objectName.c_str(), 10, 60, fontSize, allObjects[2].color);
+        DrawText(object4.objectName.c_str(), 10, 85, fontSize, allObjects[3].color);
+        DrawText(object5.objectName.c_str(), 10, 110, fontSize, allObjects[4].color);
+        if (victoryCounter == allObjects.size())
+        {
+            DrawText("YOU WIN!", (WINDOW_WIDTH / 2) - 100, WINDOW_HEIGHT / 2, 40, BLUE);
+        }
+
         EndDrawing();
     }
     CloseWindow();

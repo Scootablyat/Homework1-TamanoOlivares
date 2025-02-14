9ea6329bd1fe7d6b837366cd7932f0b93f379a78
csci40 finalproject <christianolivares13@gmail.com>
2025-02-15 03:23:39 +0800

    finished product

--- Main.cpp
+++ Main.cpp
@@ -19,23 +19,45 @@ struct object
     Color color = RED;
 };
 
-void movement(float edge_x[2], float edge_y[2], float speed, Vector2 &circle, float &circle_size)
+void movement(float edge_x[2], float edge_y[2], float speed, Vector2 &circle, Vector2 &camera, float &circle_size, bool &movement)
 {
-    if (IsKeyDown(KEY_W) && (circle.y - circle_size / 2) > edge_y[0])
+    if (movement)
     {
-        circle.y -= speed * GetFrameTime();
+        if (IsKeyDown(KEY_W) && (circle.y - circle_size / 2) > edge_y[0])
+        {
+            circle.y -= speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_S) && (circle.y + circle_size / 2) < edge_y[1])
+        {
+            circle.y += speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_A) && (circle.x - circle_size / 2) > edge_x[0])
+        {
+            circle.x -= speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_D) && (circle.x + circle_size / 2) < edge_x[1])
+        {
+            circle.x += speed * GetFrameTime();
+        }
     }
-    if (IsKeyDown(KEY_S) && (circle.y + circle_size / 2) < edge_y[1])
+    else
     {
-        circle.y += speed * GetFrameTime();
-    }
-    if (IsKeyDown(KEY_A) && (circle.x - circle_size) > edge_x[0])
-    {
-        circle.x -= speed * GetFrameTime();
-    }
-    if (IsKeyDown(KEY_D) && (circle.x + circle_size / 2) < edge_x[1])
-    {
-        circle.x += speed * GetFrameTime();
+        if (IsKeyDown(KEY_W) && (circle.y - camera.y) >= -(WINDOW_HEIGHT * .4 / 2))
+        {
+            circle.y -= speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_S) && (circle.y - camera.y) <= (WINDOW_HEIGHT * .4 / 2))
+        {
+            circle.y += speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_A) && (circle.x - camera.x) >= -(WINDOW_WIDTH * .5 / 2))
+        {
+            circle.x -= speed * GetFrameTime();
+        }
+        if (IsKeyDown(KEY_D) && (circle.x - camera.x) <= (WINDOW_WIDTH * .5 / 2))
+        {
+            circle.x += speed * GetFrameTime();
+        }
     }
 }
 
@@ -185,9 +207,9 @@ void parseSettingsFile(std::string &imageName, object &object1, object &object2,
 int main()
 {
     bool movementEnabled = true;
-    float speed = 220;
+    float speed = 440;
     int fontSize = 24;
-    Vector2 player = {(WINDOW_WIDTH / 2) + 100, (WINDOW_HEIGHT / 2) + 100};
+    Vector2 player = {(WINDOW_WIDTH / 2) + 500, (WINDOW_HEIGHT / 2) + 500};
     float playerSize = 200;
     Vector2 cameraWindowDimensions;
     float cameraDriftSpeed;
@@ -222,10 +244,7 @@ int main()
     while (!WindowShouldClose())
     {
         victoryCounter = 0;
-        // if (movementEnabled)
-        // {
-        movement(EDGE_X, EDGE_Y, speed, player, playerSize);
-        // }
+        movement(EDGE_X, EDGE_Y, speed, player, camera_view.target, playerSize, movementEnabled);
 
         if (IsKeyPressed(KEY_ENTER) && movementEnabled)
         {
@@ -241,7 +260,7 @@ int main()
         {
             for (size_t i = 0; i < allObjects.size(); i++)
             {
-                if (CheckCollisionPointRec(allObjects[i].objectPos, (Rectangle){player.x, player.y, playerSize, playerSize}))
+                if (CheckCollisionPointRec(allObjects[i].objectPos, (Rectangle){player.x - (playerSize / 2), player.y - (playerSize / 2), playerSize, playerSize}))
                 {
                     allObjects[i].isFound = true;
                     allObjects[i].color = GREEN;
@@ -269,68 +288,78 @@ int main()
 
         // Camera-Window & Position-Snapping is enabled when there's no Edge-Snapping
         bool hasNoInput = !(IsKeyDown(KEY_W) || IsKeyDown(KEY_A) || IsKeyDown(KEY_S) || IsKeyDown(KEY_D));
-        if (!(camera_view.target.x - (playerSize / 2) - defaultOffset.x <= EDGE_X[0]) && movementEnabled) // < EDGE_X Min
+        if (movementEnabled)
         {
-            if (player.x - (playerSize / 2) <= camera_view.target.x - cameraWindowDimensions.x / 2)
-                camera_view.target.x = player.x - (playerSize / 2) + cameraWindowDimensions.x / 2;
-            if (player.x < camera_view.target.x && hasNoInput)
-                camera_view.target.x -= cameraDriftSpeed * .05;
-        }
-        if (!(camera_view.target.x + defaultOffset.x >= EDGE_X[1]) && movementEnabled) // < EDGE_X Max
-        {
-            if (player.x + (playerSize / 2) >= camera_view.target.x + cameraWindowDimensions.x / 2)
-                camera_view.target.x = player.x + (playerSize / 2) - (cameraWindowDimensions.x / 2);
-            if (player.x > camera_view.target.x && hasNoInput)
-                camera_view.target.x += cameraDriftSpeed * .05;
+
+            if (camera_view.target.x < (WINDOW_WIDTH / 2)) // If you're in a position to edge-snap
+                camera_view.offset.x = defaultOffset.x;
+            else
+            {
+                if (player.x - (playerSize / 2) <= camera_view.target.x - cameraWindowDimensions.x / 2) // Camera Window
+                    camera_view.target.x = player.x - (playerSize / 2) + cameraWindowDimensions.x / 2;
+                if (std::round(player.x) < std::round(camera_view.target.x) && hasNoInput) // Position Snapping
+                    camera_view.target.x -= cameraDriftSpeed * .1;
+            }
+
+            if (camera_view.target.x > EDGE_X[1] - (WINDOW_WIDTH / 2))
+                camera_view.offset.x = defaultOffset.x;
+            else
+            {
+                if (player.x + (playerSize / 2) >= camera_view.target.x + cameraWindowDimensions.x / 2)
+                    camera_view.target.x = player.x + (playerSize / 2) - (cameraWindowDimensions.x / 2);
+                if (std::round(player.x) > std::round(camera_view.target.x) && hasNoInput)
+                    camera_view.target.x += cameraDriftSpeed * .1;
+            }
+
+            if (camera_view.target.y < (WINDOW_HEIGHT / 2))
+                camera_view.offset.y = defaultOffset.y;
+            else
+            {
+                if (player.y - (playerSize / 2) <= camera_view.target.y - (cameraWindowDimensions.y / 2))
+                    camera_view.target.y = player.y - (playerSize / 2) + cameraWindowDimensions.y / 2;
+                if (std::round(player.y) < std::round(camera_view.target.y) && hasNoInput)
+                    camera_view.target.y -= cameraDriftSpeed * .05;
+            }
+            if (camera_view.target.y > EDGE_Y[1] - (WINDOW_HEIGHT / 2))
+                camera_view.offset.y = defaultOffset.y;
+            else
+            {
+                if (player.y + (playerSize / 2) >= camera_view.target.y + cameraWindowDimensions.y / 2)
+                    camera_view.target.y = player.y + (playerSize / 2) - cameraWindowDimensions.y / 2;
+                if (player.y > camera_view.target.y && hasNoInput)
+                    camera_view.target.y += cameraDriftSpeed * .05;
+            }
         }
 
-        if (!(camera_view.target.y - defaultOffset.y <= EDGE_Y[0]) && movementEnabled) // < EDGE_Y Min
-        {
-            if (player.y - (playerSize / 2) <= camera_view.target.y - cameraWindowDimensions.y / 2)
-                camera_view.target.y = player.y - (playerSize / 2) + cameraWindowDimensions.y / 2;
-            if (player.y < camera_view.target.y && hasNoInput)
-                camera_view.target.y -= cameraDriftSpeed * .05;
-        }
-        if (!(camera_view.target.y + defaultOffset.y >= EDGE_Y[1]) && movementEnabled) // < EDGE_Y Max
-        {
-            if (player.y + (playerSize / 2) >= camera_view.target.y + cameraWindowDimensions.y / 2)
-                camera_view.target.y = player.y + (playerSize / 2) - cameraWindowDimensions.y / 2;
-            if (player.y > camera_view.target.y && hasNoInput)
-                camera_view.target.y += cameraDriftSpeed * .05;
-        }
+        // if (!(camera_view.target.x - (playerSize / 2) - defaultOffset.x <= EDGE_X[0]) && movementEnabled)
+        // {
+        //     if (player.x - (playerSize / 2) <= camera_view.target.x - cameraWindowDimensions.x / 2)
+        //         camera_view.target.x = player.x - (playerSize / 2) + cameraWindowDimensions.x / 2;
+        //     if (player.x < camera_view.target.x && hasNoInput)
+        //         camera_view.target.x -= cameraDriftSpeed * .05;
+        // }
+        // if (!(camera_view.target.x + defaultOffset.x >= EDGE_X[1]) && movementEnabled) // < EDGE_X Max
+        // {
+        //     if (player.x + (playerSize / 2) >= camera_view.target.x + cameraWindowDimensions.x / 2)
+        //         camera_view.target.x = player.x + (playerSize / 2) - (cameraWindowDimensions.x / 2);
+        //     if (player.x > camera_view.target.x && hasNoInput)
+        //         camera_view.target.x += cameraDriftSpeed * .05;
+        // }
 
-        /*
-         */
-
-        // std::cout << camera_view.target.x << " " << camera_view.target.y<< std::endl;
-        /*
-        if(player.x - defaultOffset.x <= EDGE_X[0]){
-            //camera_view.target.x = camera_view.target.x;
-            camera_view.offset.x = camera_view.target.x + abs(EDGE_X[0] - camera_view.target.x) - defaultOffset.x;
-        }
-        if(player.x + defaultOffset.x >= EDGE_X[1]){
-            //camera_view.target.x = camera_view.target.x;
-            camera_view.offset.x = camera_view.target.x - abs(EDGE_X[0] - camera_view.target.x) + defaultOffset.x;
-            //camera_view.offset.x -= abs(EDGE_X[1] - camera_view.target.x);
-        }
-        if(player.y - defaultOffset.y <= EDGE_Y[0]){
-            //camera_view.target.y = camera_view.target.y;
-            camera_view.offset.y = camera_view.target.y + abs(EDGE_Y[0] - camera_view.target.y) - defaultOffset.y;
-        }
-        if(player.y + defaultOffset.y >= EDGE_Y[1]){
-            //camera_view.target.y = camera_view.target.y;
-            camera_view.offset.y = camera_view.target.y - abs(EDGE_Y[0] - camera_view.target.y) + defaultOffset.y;
-            //camera_view.offset.y -= abs(EDGE_Y[1] - camera_view.target.y);
-        }
-        if(!(player.x - defaultOffset.x <= EDGE_X[0] || player.x + defaultOffset.x >= EDGE_X[1])){
-            camera_view.target.x = player.x;
-            // camera_view.offset.x = defaultOffset.x;
-        }
-        if(!(player.y - defaultOffset.y <= EDGE_Y[0] || player.y + defaultOffset.y >= EDGE_Y[1])){
-            camera_view.target.y = player.y;
-            // camera_view.offset.y = defaultOffset.y;
-        }
-        */
+        // if (!(camera_view.target.y - defaultOffset.y <= EDGE_Y[0]) && movementEnabled) // < EDGE_Y Min
+        // {
+        //     if (player.y - (playerSize / 2) <= camera_view.target.y - cameraWindowDimensions.y / 2)
+        //         camera_view.target.y = player.y - (playerSize / 2) + cameraWindowDimensions.y / 2;
+        //     if (player.y < camera_view.target.y && hasNoInput)
+        //         camera_view.target.y -= cameraDriftSpeed * .05;
+        // }
+        // if (!(camera_view.target.y + defaultOffset.y >= EDGE_Y[1]) && movementEnabled) // < EDGE_Y Max
+        // {
+        //     if (player.y + (playerSize / 2) >= camera_view.target.y + cameraWindowDimensions.y / 2)
+        //         camera_view.target.y = player.y + (playerSize / 2) - cameraWindowDimensions.y / 2;
+        //     if (player.y > camera_view.target.y && hasNoInput)
+        //         camera_view.target.y += cameraDriftSpeed * .05;
+        // }
 
         // Draw Step
         BeginDrawing();
@@ -338,19 +367,9 @@ int main()
 
         ClearBackground(BLACK);
         DrawTexture(texture, 0, 0, WHITE);
-        if (movementEnabled)
-            DrawRectangleLines((camera_view.target.x) - cameraWindowDimensions.x / 2, (camera_view.target.y) - cameraWindowDimensions.y / 2, cameraWindowDimensions.x, cameraWindowDimensions.y, RED);
+        // if (movementEnabled)
+        //     DrawRectangleLines((camera_view.target.x) - cameraWindowDimensions.x / 2, (camera_view.target.y) - cameraWindowDimensions.y / 2, cameraWindowDimensions.x, cameraWindowDimensions.y, RED);
         DrawRectangleLines(player.x - (playerSize / 2), player.y - (playerSize / 2), playerSize, playerSize, PINK);
-        // DrawText(object1.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150, fontSize, allObjects[0].color);
-        // DrawText(object2.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150 + fontSize + 1, fontSize, allObjects[1].color);
-        // DrawText(object3.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150 + ((fontSize + 1) * 2), fontSize, allObjects[2].color);
-        // DrawText(object4.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150 + ((fontSize + 1) * 3), fontSize, allObjects[3].color);
-        // DrawText(object5.objectName.c_str(), camera_view.target.x - cameraWindowDimensions.x / 2 - 200, (camera_view.target.y) - (cameraWindowDimensions.y / 2) - 150 + ((fontSize + 1) * 4), fontSize, allObjects[4].color);
-
-        // if (victoryCounter == allObjects.size())
-        // {
-        //     DrawText("YOU WIN!", (camera_view.target.x) - cameraWindowDimensions.x / 2, (camera_view.target.y) - cameraWindowDimensions.y / 2, 40, BLUE);
-        // }
 
         EndMode2D();
 
